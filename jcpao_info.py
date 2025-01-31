04d2a4efbd8e0663aa2aea8dce367b7fb74e524c
Joseph Cho <ujcho@jacksongov.org>
2025-01-31 17:05:11 -0600

    Added hyperlink to official website

--- jcpao_info.py
+++ jcpao_info.py
@@ -1,2 +1,5 @@
 import streamlit as st
 
+jcpao_link = "https://www.jacksoncountyprosecutor.com/"
+csu_link = "https://www.jacksoncountyprosecutor.com/232/Crime-Strategies-Unit"
+st.write(f"Built by the (Crime Strategies Unit)[{csu_link}] of the (Jackson County Prosecuting Attorney's Office)[{jcpao_link}].")
